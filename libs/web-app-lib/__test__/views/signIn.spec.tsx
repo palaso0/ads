bc711dd3f2afe355553341b4322b5fdecf782bc3
Paulo Choque <Paulo.Choque@fundacion-jala.org>
2022-09-26 23:12:10 -0400

    first tests added

--- libs/web-app-lib/__test__/views/signIn.spec.tsx
+++ libs/web-app-lib/__test__/views/signIn.spec.tsx
@@ -1,15 +1,21 @@
 import SignIn from '../../views/signIn'
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import AppWrapper from '../utils/appWrapper'
 
 import '@testing-library/jest-dom/extend-expect'
 
-describe('SignIn', () => {
-  it('should render the component SignUp', () => {
-
-    const component = render(<AppWrapper Component={SignIn} />);
-
-    expect(component).toMatchSnapshot();
-
+describe('SignIn render components', () => {
+  render(<AppWrapper Component={SignIn} />);
+  
+  it('should render the component title SignUp', () => {
+    const title = screen.getByText("Sign in")
+    expect(title).toBeInTheDocument();
   });
+  it('should render the component input Email Address', () => {
+    const title = screen.getAllByText("Email")
+    console.log(title);
+    screen.debug(screen.getAllByText("Email"))
+    
+  });
+  
 })
