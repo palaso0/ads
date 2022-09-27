80d5dbd8150f4eace393d4e84bebb6f0f994f45c
Paulo Choque <Paulo.Choque@fundacion-jala.org>
2022-09-26 23:26:00 -0400

    beforeEach (test) added

--- libs/web-app-lib/__test__/views/signIn.spec.tsx
+++ libs/web-app-lib/__test__/views/signIn.spec.tsx
@@ -4,18 +4,29 @@ import AppWrapper from '../utils/appWrapper'
 
 import '@testing-library/jest-dom/extend-expect'
 
-describe('SignIn render components', () => {
+beforeEach(()=>{
   render(<AppWrapper Component={SignIn} />);
+})
+
+describe('SignIn render components', () => {
+  
   
   it('should render the component title SignUp', () => {
     const title = screen.getByText("Sign in")
     expect(title).toBeInTheDocument();
   });
   it('should render the component input Email Address', () => {
-    const title = screen.getAllByText("Email")
-    console.log(title);
-    screen.debug(screen.getAllByText("Email"))
-    
+    const title = screen.getByText("Email Address")
+    expect(title).toBeInTheDocument();
+  });
+  it('should render the component input Password', () => {
+    const title = screen.getByText("Password")
+    expect(title).toBeInTheDocument();
+  });
+  it('should render the button component SIGN IN', () => {
+    //TODO
+    const btnEl = screen.getByRole('button',{name: /password/i})
+    expect(btnEl).toBeInTheDocument();
   });
   
 })
